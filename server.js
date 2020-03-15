05e73679ced3174357cd7577bc4b1e907be5b8dd
ZaneMeroff <53405028+ZaneMeroff@users.noreply.github.com>
2020-03-15 08:57:28 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,26 +2,28 @@ const http = require("http");
 const url = require('url');
 const server = http.createServer();
 
-server.listen(3000, () => {
-  console.log('The HTTP server is listening at Port 3000.');
-});
-
-server.on('request', (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('Hello World\n');
-  response.end();
-});
-
 let messages = [
   { 'id': 1, 'user': 'brittany storoz', 'message': 'hi there!' },
   { 'id': 2, 'user': 'bob loblaw', 'message': 'check out my law blog' },
   { 'id': 3, 'user': 'lorem ipsum', 'message': 'dolor set amet' }
 ];
 
-const getAllMessages = () => {
-  console.log(messages);
-  return messages
+const getAllMessages = (response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.write(JSON.stringify(messages));
+  response.end();
 }
+
+const addMessage = (message, response) => {
+  messages.push(message)
+  response.writeHead(201, { 'Content-Type': 'text/plain' });
+  response.write(JSON.stringify(message));
+  response.end();
+}
+
+server.listen(3000, () => {
+  console.log('The HTTP server is listening at Port 3000.');
+});
 
 server.on('request', (request, response) => {
   if (request.method === 'GET') {
